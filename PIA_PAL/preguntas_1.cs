abf66777c3230246e2a872259e0b04de739a223d
andresTorresmr <andrestorresm001@gmail.com>
2022-11-05 12:18:18 -0600

    Prueba pls

--- PIA_PAL/preguntas_1.cs
+++ PIA_PAL/preguntas_1.cs
@@ -26,158 +26,133 @@ namespace PIA_PAL
             if (result == DialogResult.Yes)
             {
                 //PREGUNTA 1
-                if (rbLti.Checked == false && rbLni.Checked == false && rbLa.Checked == false && rbCp.Checked == false)
+                if (rbLti.Checked == false && rbLni.Checked == false && rbLa.Checked == false && rbCp.Checked == false ||
+                    rbLti_1.Checked == false && rbLni_1.Checked == false && rbLa_1.Checked == false && rbCp_1.Checked == false ||
+                    rbLti_2.Checked == false && rbLni_2.Checked == false && rbLa_2.Checked == false && rbCp_2.Checked == false ||
+                    rbLti_3.Checked == false && rbLni_3.Checked == false && rbLa_3.Checked == false && rbCp_3.Checked == false ||
+                    rbLti_4.Checked == false && rbLni_4.Checked == false && rbLa_4.Checked == false && rbCp_4.Checked == false)
                 {
-                    MessageBox.Show("Contesta la primer pregunta");
+                    MessageBox.Show("Contesta todas las preguntas");
                 }
                 else
                 {
                     if (rbLti.Checked)
                     {
                         Variables.lti += 5;
-                        MessageBox.Show("LTI: " + Variables.lti);
+                        
                     }
                     if (rbLni.Checked)
                     {
                         Variables.lni += 5;
-                        MessageBox.Show("LNI: " + Variables.lni);
+                        
                     }
                     if (rbLa.Checked)
                     {
                         Variables.la += 5;
-                        MessageBox.Show("LA: " + Variables.la);
+                        
                     }
                     if (rbCp.Checked)
                     {
                         Variables.cp += 5;
-                        MessageBox.Show("CP: " + Variables.cp);
+                        
                     }
                     groupBox1.Enabled = false;
-                    
-                }
 
-                //PREGUNTA 2
-                if (rbLti_1.Checked == false && rbLni_1.Checked == false && rbLa_1.Checked == false && rbCp_1.Checked == false)
-                {
-                    MessageBox.Show("Contesta la segunda pregunta");
-                }
-                else
-                {
                     if (rbLti_1.Checked)
                     {
                         Variables.lti += 5;
-                        MessageBox.Show("LTI: " + Variables.lti);
+                        
                     }
                     if (rbLni_1.Checked)
                     {
                         Variables.lni += 5;
-                        MessageBox.Show("LNI: " + Variables.lni);
+                        
                     }
                     if (rbLa_1.Checked)
                     {
                         Variables.la += 5;
-                        MessageBox.Show("LA: " + Variables.la);
+                        
                     }
                     if (rbCp_1.Checked)
                     {
                         Variables.cp += 5;
-                        MessageBox.Show("CP: " + Variables.cp);
+                        
                     }
                     groupBox2.Enabled = false;
-                }
 
-                //PREGUNTA 3
-                if (rbLti_2.Checked == false && rbLni_2.Checked == false && rbLa_2.Checked == false && rbCp_2.Checked == false)
-                {
-                    MessageBox.Show("Contesta la tercera pregunta");
-                }
-                else
-                {
                     if (rbLti_2.Checked)
                     {
                         Variables.lti += 5;
-                        MessageBox.Show("LTI: " + Variables.lti);
+                        
                     }
                     if (rbLni_2.Checked)
                     {
                         Variables.lni += 5;
-                        MessageBox.Show("LNI: " + Variables.lni);
+                        
                     }
                     if (rbLa_2.Checked)
                     {
                         Variables.la += 5;
-                        MessageBox.Show("LA: " + Variables.la);
+                        
                     }
                     if (rbCp_2.Checked)
                     {
                         Variables.cp += 5;
-                        MessageBox.Show("CP: " + Variables.cp);
+                        
                     }
 
                     groupBox3.Enabled = false;
 
-                }
-
-                //PREGUNTA 4
-                if (rbLti_3.Checked == false && rbLni_3.Checked == false && rbLa_3.Checked == false && rbCp_3.Checked == false)
-                {
-                    MessageBox.Show("Contesta la cuarta pregunta");
-                }
-                else
-                {
                     if (rbLti_3.Checked)
                     {
                         Variables.lti += 5;
-                        MessageBox.Show("LTI: " + Variables.lti);
+                        
                     }
                     if (rbLni_3.Checked)
                     {
                         Variables.lni += 5;
-                        MessageBox.Show("LNI: " + Variables.lni);
+                       
                     }
                     if (rbLa_3.Checked)
                     {
                         Variables.la += 5;
-                        MessageBox.Show("LA: " + Variables.la);
+                        
                     }
                     if (rbCp_3.Checked)
                     {
                         Variables.cp += 5;
-                        MessageBox.Show("CP: " + Variables.cp);
+                       
                     }
 
                     groupBox4.Enabled = false;
-                }
 
-                //PREGUNTA 5
-                if (rbLti_4.Checked == false && rbLni_4.Checked == false && rbLa_4.Checked == false && rbCp_4.Checked == false)
-                {
-                    MessageBox.Show("Contesta la quinta pregunta");
-                }
-                else
-                {
                     if (rbLti_4.Checked)
                     {
                         Variables.lti += 5;
-                        MessageBox.Show("LTI: " + Variables.lti);
+                       
                     }
                     if (rbLni_4.Checked)
                     {
                         Variables.lni += 5;
-                        MessageBox.Show("LNI: " + Variables.lni);
+                       
                     }
                     if (rbLa_4.Checked)
                     {
                         Variables.la += 5;
-                        MessageBox.Show("LA: " + Variables.la);
+                        
                     }
                     if (rbCp_4.Checked)
                     {
                         Variables.cp += 5;
-                        MessageBox.Show("CP: " + Variables.cp);
+                        
                     }
 
                     groupBox5.Enabled = false;
+
+                    MessageBox.Show("LTI: " + Variables.lti);
+                    MessageBox.Show("LNI: " + Variables.lni);
+                    MessageBox.Show("LA: " + Variables.la);
+                    MessageBox.Show("CP: " + Variables.cp);
                 }
 
                 
